7ed74213f9434d906078661d59b42f02ac5d6141
DebRC <roychowdhurydebdoot@gmail.com>
2023-10-06 16:32:15 +0530

    Modified login function

--- static/customer/script.js
+++ static/customer/script.js
@@ -1,26 +1,35 @@
 // Function to submit the login form
 function submitLoginForm(event) {
-    event.preventDefault(); // Prevent the default form submission behavior
+    event.preventDefault();
 
-    // Get form data
     const form = document.getElementById('login-form');
-    const formData = new FormData(form);
 
-    // Make an API request (replace with your actual API endpoint)
+    const username = form.querySelector('#username').value;
+    const password = form.querySelector('#password').value;
+
+    form.reset();
+
     fetch('/api/login', {
-        method: 'POST', // or 'GET' if it's a GET request
-        body: formData,
+        method: 'POST',
+        body: JSON.stringify({ "username": username, "password": password }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
     })
         .then(response => response.json())
         .then(data => {
-            // Handle API response here
-            console.log(data);
-            // You can redirect to another page or display a success message
+            if(data["res"]==0){
+                alert(data["message"])
+            }
+            else{
+                alert(data["message"])
+            }
         })
         .catch(error => {
             console.error('Error:', error);
-            // Handle error, e.g., display an error message to the user
+            alert(error);
         });
+    form.reset();
 }
 
 // Function to submit the signup form
@@ -31,8 +40,6 @@ function submitSignupForm(event) {
 
     const username = form.querySelector('#username').value;
     const password = form.querySelector('#password').value;
-
-    console.log(username)
 
     fetch('/api/signup', {
         method: 'POST',
@@ -45,16 +52,15 @@ function submitSignupForm(event) {
         .then(data => {
             console.log(data);
             if(data["res"]==0){
-                alert("Sign Up Failure")
+                alert(data["message"])
             }
             else{
-                alert("Sign Up Successful")
+                alert(data["message"])
             }
         })
         .catch(error => {
             console.error('Error:', error);
             alert(error);
         });
-
-
+    form.reset();
 }
