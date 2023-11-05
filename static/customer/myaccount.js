8ba774ec8796b1e9a775eb6c2de45347398bf6f3
Debdoot Roy <debrc@cse.iitb.ac.in>
2023-11-05 10:46:33 +0530

    Added alert for logged out

--- static/customer/myaccount.js
+++ static/customer/myaccount.js
@@ -3,12 +3,18 @@ console.log(accessToken);
 // Fetch the /myaccount route with the JWT token in the Authorization header
 fetch(`/myaccount?accessToken=${accessToken}`, {
     method: 'GET',
-    headers: {
-        'Authorization': `Bearer ${accessToken}`,
-    },
 })
-  .then((response) => response.text())
+.then((response) => {
+    if (!response.ok) {
+        alert("User logged out");
+        window.location.href = '/';
+    }
+    return response.text();
+  })
   .then((data) => {
     document.body.innerHTML = data;
   })
-  .catch((error) => console.error("Error:", error));
+  .catch((error) => {
+    console.error("Error:", error);
+    alert("Failed to fetch data. Please try again.");
+  });
