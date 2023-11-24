77752b8d62a4791dfefa378d46104aaee3afe628
DebRC <roychowdhurydebdoot@gmail.com>
2023-11-24 23:42:39 +0530

    Added redirectToOrder function

--- static/customer/myaccount.js
+++ static/customer/myaccount.js
@@ -18,3 +18,22 @@ fetch(`/myaccount?accessToken=${accessToken}`, {
     console.error("Error:", error);
     alert("Failed to fetch data. Please try again.");
   });
+
+function redirectToOrder(event) {
+  // Check if the access token exists
+  if (accessToken) {
+    // Get the clicked anchor tag
+    var anchorTag = event.target;
+
+    // Get the order ID from the data attribute
+    var orderId = anchorTag.getAttribute('data-order-id');
+
+    // Construct the redirect URL
+    var redirectUrl = '/myorder/' + accessToken + '/' +  orderId;
+
+    // Redirect to the constructed URL
+    window.location.href = redirectUrl;
+  } else {
+    console.error('Access token not found in local storage');
+  }
+}
