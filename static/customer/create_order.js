cf91c481948c652749a68961d373763d6f19cddf
DebRC <roychowdhurydebdoot@gmail.com>
2023-11-25 01:19:34 +0530

    Added name as param

--- static/customer/create_order.js
+++ static/customer/create_order.js
@@ -1,22 +1,22 @@
 // Function to add an item to the cart
-function addToCart(productId) {
+function addToCart(id, name) {
     var quantitySelector = document.getElementById('quantitySelector');
 
     // Get the selected value
-    var quantity = parseInt(quantitySelector.value,10);
-    var cartData = JSON.parse(localStorage.getItem('cart'));
+    var qty = parseInt(quantitySelector.value,10);
+    var cartData = JSON.parse(localStorage.getItem('cart') || []);
     // Check if the product already exists in the cart
-    var existingItemIndex = cartData.findIndex(item => item.productId === productId);
+    var existingItemIndex = cartData.findIndex(item => item.id === id);
   
     if (existingItemIndex !== -1) {
       // If the product exists, update the quantity
-      cartData[existingItemIndex].quantity += quantity;
+      cartData[existingItemIndex].qty += qty;
     } else {
       // If the product doesn't exist, add a new item to the cart
-      cartData.push({ id: productId, qty: quantity });
+      cartData.push({ id: id, name: name, qty: qty });
     }
     alert("Added to Cart");
-    // alert(JSON.stringify(cartData));
+    alert(JSON.stringify(cartData));
     // Save the updated cart data back to local storage
     localStorage.setItem('cart', JSON.stringify(cartData));
   }
