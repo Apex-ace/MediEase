2ec1daebbce81d063273eb202a923997f3fdc81d
DebRC <roychowdhurydebdoot@gmail.com>
2023-11-25 03:13:27 +0530

    Improved buy now function

--- static/customer/cart.js
+++ static/customer/cart.js
@@ -1,11 +1,40 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var cartData = JSON.parse(localStorage.getItem('cart'));
-    var cartList = document.getElementById('cartList');
-  
+  var cartData = JSON.parse(localStorage.getItem('cart'));
+  var cartList = document.getElementById('cartList');
+  if (cartData.length === 0) {
+    listItem.textContent = 'Your Cart is Empty';
+  }
+  else {
     cartData.forEach(function (item) {
       var listItem = document.createElement('li');
-      listItem.textContent = 'Name: ' + item.name + ', Quantity: ' + item.qty;
+      listItem.textContent = 'Product Name: ' + item.name + ', Quantity: ' + item.qty;
       cartList.appendChild(listItem);
     });
-  });
-  
+  }
+});
+
+// Function to buy an item to the cart
+function buyNow() {
+  const accessToken = localStorage.getItem('accessToken');
+  if (accessToken) {
+    var cartData = localStorage.getItem('cart');
+    if (cartData) {
+      cartData = JSON.parse(cartData);
+      if (cartData.length === 0) {
+        alert("Cart is Empty. Add some medicines to order");
+        window.location.href = '/';
+      }
+      else{
+        window.location.href = '/createOrder';
+      }
+    }
+    else {
+      alert("Cart is Empty. Add some medicines to order");
+      window.location.href = '/';
+    }
+  } else {
+    alert("User is logged out. Login Again");
+    window.location.href = '/login';
+  }
+
+}
